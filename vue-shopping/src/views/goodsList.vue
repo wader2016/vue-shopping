fd9ece5c6d8a9d02b97b74f5bd1205658494f9a8
1481234405@qq.com <33a44b77d99c>
2018-12-14 23:01:44 +0800

    测试2

---
+++ vue-shopping/src/views/goodsList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <nav-header></nav-header>
+    <nav-bread>
+      <span>goods</span>
+    </nav-bread>
+    <nav-footer></nav-footer>
+  </div>
+</template>
+
+<script>
+import NavHeader from '../components/NavHeader'
+import NavFooter from '../components/NavFooter'
+import NavBread from '../components/NavBread'
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components:{
+    NavHeader,
+    NavFooter,
+    NavBread
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
